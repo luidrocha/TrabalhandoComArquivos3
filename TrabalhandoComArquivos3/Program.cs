af21b43c73fc2739f061503542fe1e83fff038f4
Luidrocha <luidrocha@yahoo.com.br>
2021-12-06 19:33:11 -0300

    Trabalhanco com Arquivos usando a bloco USING CSHARP

---
+++ TrabalhandoComArquivos3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace TrabalhandoComArquivos3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\File1.txt";
+
+            try
+            {
+                // o Comando using fecha automaticamentes as classes que não são controladas pelo CLI                                                                                                                                                                                                                         
+                //  using (FileStream fs = new FileStream(path, FileMode.Open))
+                
+                    using (StreamReader sr = File.OpenText(path))  //Simplificando usa-se o File
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            string linha = sr.ReadLine();
+                            Console.WriteLine(linha);
+                        }
+
+
+                    }
+                
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("Ocorreu erro no arquivo: ");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
